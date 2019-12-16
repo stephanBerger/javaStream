32e97fa884de5ae92053c52b54ec24639779cbce
sBerger <stephone13@gmail.com>
2019-12-16 20:42:28 +0100

    ObjectTraining ok

--- src/exercices/ObjectTraining.java
+++ src/exercices/ObjectTraining.java
@@ -1,38 +1,67 @@
 public class ObjectTraining {
 
-    // TODO 1. Add the following private attributes :
-    // - name (String)
-    // - age (int)
-    // - wilder (boolean)
+	// TODO 1. Add the following private attributes :
+	// - name (String)
+	private String name;
+	// - age (int)
+	private int age;
+	// - wilder (boolean)
+	private boolean wilder;
 
+	// TODO 2. Add a Constructor with name and age arguments (in this order)
+	// This constructor must initialize name and age attributes
 
-    // TODO 2. Add a Constructor with name and age arguments (in this order)
-    // This constructor must initialize name and age attributes
+	// TODO 3. Add getter method for name attribute
 
+	// TODO 4. Add setter method for name attribute
 
-    // TODO 3. Add getter method for name attribute
+	// TODO 5. Add getter method for age attribute
 
+	// TODO 6. Add setter method for age attribute
 
-    // TODO 4. Add setter method for name attribute
+	// TODO 7. Add getter method for wilder attribute
 
+	// TODO 8. Add setter method for wilder attribute
 
-    // TODO 5. Add getter method for age attribute
+	// TODO 9. Create a method 'whoAmI' that returns "My name is {name} and I'm
+	// {age}"
+	// where you replace {name} and {age} with attributes values
 
+	// Do not remove this empty constructor !
+	public ObjectTraining() {
+	}
 
-    // TODO 6. Add setter method for age attribute
+	public ObjectTraining(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
 
+	public String whoAmI() {
+		return "My name is " + this.name + " and I'm " + this.age;
+	}
 
-    // TODO 7. Add getter method for wilder attribute
+	public String getName() {
+		return this.name;
+	}
 
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    // TODO 8. Add setter method for wilder attribute
+	public int getAge() {
+		return this.age;
+	}
 
+	public void setAge(int age) {
+		this.age = age;
+	}
 
-    // TODO 9. Create a method 'whoAmI' that returns "My name is {name} and I'm {age}"
-    // where you replace {name} and {age} with attributes values
+	public boolean isWilder() {
+		return this.wilder;
+	}
 
-
-    // Do not remove this empty constructor !
-    public ObjectTraining() {
-    }
+	public void setWilder(boolean wilder) {
+		this.wilder = wilder;
+	}
 }
